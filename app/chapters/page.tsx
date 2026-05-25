2f7e3e592bdf51dec1145b56a0ed9e4893cdda4c
Rodrigues Norvin Earnest <norvinearnestrodrigues@gmail.com>
2026-05-25 23:20:29 +0530

    removed the download button

--- app/chapters/page.tsx
+++ app/chapters/page.tsx
@@ -58,29 +58,6 @@ export default async function ChaptersPage() {
                     >
                       READ
                     </Link>
-                    {chapter.downloadUrl && (
-                      <Link
-                      
-                        href={`/download/ch/${chapter.chapterNumber}`}
-                        className="p-2 bg-neutral-900 border border-neutral-800 rounded hover:bg-neutral-800 text-neutral-400 hover:text-white transition"
-                        title="Download Zip File"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-                          />
-                        </svg>
-                      </Link>
-                    )}
                   </div>
                 </div>
               </div>
